6c3f5fd771b2f6a21f3fca0ef5c03aad9bdf3834
Diogu <Diogu@LAPTOP-5QN1PRTU>
2023-04-03 21:01:59 +0100

    Adicionar arquivos de projeto.

---
+++ NumberGenerator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace RandomNumberGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Generate a random number between 1 and 100
+            Random random = new Random();
+            int secretNumber = random.Next(1, 101);
+
+            int guess = 0;
+            int attempts = 0;
+
+            Console.WriteLine("Guess the secret number between 1 and 100!");
+
+            do
+            {
+                Console.Write("Enter your guess: ");
+                guess = int.Parse(Console.ReadLine());
+                attempts++;
+
+                if (guess < secretNumber)
+                {
+                    Console.WriteLine("Too low, try again.");
+                }
+                else if (guess > secretNumber)
+                {
+                    Console.WriteLine("Too high, try again.");
+                }
+
+            } while (guess != secretNumber);
+
+            Console.WriteLine($"Congratulations, you guessed the secret number in {attempts} attempts!");
+        }
+    }
+}
